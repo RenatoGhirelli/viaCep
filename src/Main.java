5ebf6f6ea96ee2315ee1599a86335f4e90d4ffa4
Renato Ghirelli <renato.ghirelli@gmail.com>
2025-05-11 14:24:25 -0300

    busca e json

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -38,12 +39,18 @@ public class Main {
             String json = response.body();
             System.out.println(json);
 
+            FileWriter escrita = new FileWriter(busca+".json");
+            escrita.write(gson.toJson(json));
+            escrita.close();
+
         } catch (NumberFormatException e) {
             System.out.println("Aconteceu um erro: ");
             System.out.println(e.getMessage());
         } catch (IllegalArgumentException e) {
             System.out.println("Algum erro de argumento na busca, verifique o endereço");
         }
+
+
 
 
     }
